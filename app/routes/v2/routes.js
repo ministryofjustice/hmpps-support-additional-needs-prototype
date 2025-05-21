3d6340a32fb4dac08bfa817f6b603c00a9299244
Westley Knight <westley.knight1@justice.gov.uk>
2025-05-21 12:49:24 +0100

    Creat plan journey up to check answers

--- app/routes/v2/routes.js
+++ app/routes/v2/routes.js
@@ -1,12 +1,51 @@
-var version = "/v2/";
+var v = "/v2/";
 
 module.exports = function(router) {
 
-/**
-  router.post(version + 'url', function (req, res) {
-    if (req.session.data['var_name'] == "Yes")
-  {
-*/
-  module.exports = router
+  router.post(v + "san/plan/create/person-who-met", function (req, res) {
+    res.redirect(v + "san/plan/create/other-people-consulted");
+  });
+
+  router.post(v + "san/plan/create/other-people-consulted", function (req, res) {
+    res.redirect(v + "san/plan/create/review-needs");
+  });
+
+  router.post(v + "san/plan/create/review-needs", function (req, res) {
+    res.redirect(v + "san/plan/create/teaching-adjustments");
+  });
+
+  router.post(v + "san/plan/create/teaching-adjustments", function (req, res) {
+    res.redirect(v + "san/plan/create/environment-adjustments");
+  });
+
+  router.post(v + "san/plan/create/environment-adjustments", function (req, res) {
+    res.redirect(v + "san/plan/create/knowledge-skills");
+  });
+
+  router.post(v + "san/plan/create/knowledge-skills", function (req, res) {
+    res.redirect(v + "san/plan/create/exams-assessments");
+  });
+
+  router.post(v + "san/plan/create/exams-assessments", function (req, res) {
+    res.redirect(v + "san/plan/create/ehcp");
+  });
+
+  router.post(v + "san/plan/create/ehcp", function (req, res) {
+    res.redirect(v + "san/plan/create/lnsp-support");
+  });
+
+  router.post(v + "san/plan/create/lnsp-support", function (req, res) {
+    res.redirect(v + "san/plan/create/review-date");
+  });
+
+  router.post(v + "san/plan/create/review-date", function (req, res) {
+    res.redirect(v + "san/plan/create/check-answers");
+  });
+
+  router.post(v + "san/plan/create/check-answers", function (req, res) {
+    res.redirect(v + "san/plan/profile/?esp=created");
+  });
+
+  module.exports = router;
 
 }
