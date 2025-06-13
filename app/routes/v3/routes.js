eaaf1fce940c6307c983ceb6ee5f2fdcf90856e8
Westley Knight <westley.knight1@justice.gov.uk>
2025-06-13 12:11:44 +0100

    Ability to add support recommendations
    Also disabled the link to create education support plan as currently unfinished journey.

--- app/routes/v3/routes.js
+++ app/routes/v3/routes.js
@@ -1,4 +1,12 @@
-var v = "/v3/";
+var v = "v3";
+
+function matchref(req){
+  // find matching ref in session data
+  objIndex = req.session.data["prisoners"].findIndex(obj => obj.prisonerNumber === req.params.ref);
+  // store selected ref in a ession variable
+  let ref = req.params.ref;
+  return ref;
+}
 
 module.exports = function(router) {
 
@@ -12,74 +20,109 @@ module.exports = function(router) {
     next();
   });
 
-
-  router.get(v+"san/profile/:ref", function (req, res) {
-
-    // find matching ref in session data
-    objIndex = req.session.data["prisoners"].findIndex(obj => obj.prisonerNumber === req.params.ref);
-
-    // store selected ref in a ession variable
-    let ref = req.params.ref;
-
-    res.render(v + "san/profile/overview", {ref});
+  router.get("/"+v+"/san/:ref/profile/", function (req, res) {
+    let ref = matchref(req);
+    res.render("/"+v+"/san/profile/overview", {ref});
   });
 
 
-  router.get(v+"san/plan/:ref/create/person-who-met", function (req, res) {
+/**
+ * Create education support plan
+ */
 
-    // find matching ref in session data
-    objIndex = req.session.data["prisoners"].findIndex(obj => obj.prisonerNumber === req.params.ref);
-
-    // store selected ref in a ession variable
-    let ref = req.params.ref;
-
-    res.render(v + "san/plan/create/person-who-met", {ref});
+  router.get("/"+v+"/san/:ref/plan/create/person-who-met", function (req, res) {
+    let ref = matchref(req);
+    res.render("/"+v+"/san/plan/create/person-who-met", {ref});
   });
 
 
-  router.post(v + "san/plan/create/person-who-met", function (req, res) {
-    res.redirect(v + "san/plan/create/other-people-consulted");
+  router.post("/"+v+"/san/plan/create/person-who-met", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/other-people-consulted");
   });
 
-  router.post(v + "san/plan/create/other-people-consulted", function (req, res) {
-    res.redirect(v + "san/plan/create/review-needs");
+  router.post("/"+v+"/san/plan/create/other-people-consulted", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/review-needs");
   });
 
-  router.post(v + "san/plan/create/review-needs", function (req, res) {
-    res.redirect(v + "san/plan/create/teaching-adjustments");
+  router.post("/"+v+"/san/plan/create/review-needs", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/teaching-adjustments");
   });
 
-  router.post(v + "san/plan/create/teaching-adjustments", function (req, res) {
-    res.redirect(v + "san/plan/create/environment-adjustments");
+  router.post("/"+v+"/san/plan/create/teaching-adjustments", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/environment-adjustments");
   });
 
-  router.post(v + "san/plan/create/environment-adjustments", function (req, res) {
-    res.redirect(v + "san/plan/create/knowledge-skills");
+  router.post("/"+v+"/san/plan/create/environment-adjustments", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/knowledge-skills");
   });
 
-  router.post(v + "san/plan/create/knowledge-skills", function (req, res) {
-    res.redirect(v + "san/plan/create/exams-assessments");
+  router.post("/"+v+"/san/plan/create/knowledge-skills", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/exams-assessments");
   });
 
-  router.post(v + "san/plan/create/exams-assessments", function (req, res) {
-    res.redirect(v + "san/plan/create/ehcp");
+  router.post("/"+v+"/san/plan/create/exams-assessments", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/ehcp");
   });
 
-  router.post(v + "san/plan/create/ehcp", function (req, res) {
-    res.redirect(v + "san/plan/create/lnsp-support");
+  router.post("/"+v+"/san/plan/create/ehcp", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/lnsp-support");
   });
 
-  router.post(v + "san/plan/create/lnsp-support", function (req, res) {
-    res.redirect(v + "san/plan/create/review-date");
+  router.post("/"+v+"/san/plan/create/lnsp-support", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/review-date");
   });
 
-  router.post(v + "san/plan/create/review-date", function (req, res) {
-    res.redirect(v + "san/plan/create/check-answers");
+  router.post("/"+v+"/san/plan/create/review-date", function (req, res) {
+    res.redirect("/"+v+"/san/plan/create/check-answers");
   });
 
-  router.post(v + "san/plan/create/check-answers", function (req, res) {
-    res.redirect(v + "san/profile/");
+  router.post("/"+v+"/san/plan/create/check-answers", function (req, res) {
+    res.redirect("/"+v+"/san/profile/");
   });
+
+
+/************************
+ * Add support strategy *
+ ************************/
+
+  router.get("/"+v+"/san/:ref/support/add/category", function (req, res){
+    let ref = matchref(req);
+    res.render("/"+v+"/san/support/add/category", {ref});
+  });
+
+  router.post("/"+v+"/san/:ref/support/add/category", function (req, res) {
+    let ref = matchref(req);
+    res.redirect("/"+v+"/san/"+ref+"/support/add/description");
+  });
+
+  router.get("/"+v+"/san/:ref/support/add/description", function (req, res){
+    let ref = matchref(req);
+    res.render("/"+v+"/san/support/add/description", {ref});
+  });
+
+  router.post("/"+v+"/san/:ref/support/add/description", function (req, res) {
+    let ref = matchref(req);
+
+    //letNewSupportCat = req.session.data["san-"+v+"-"+ref+"-support-category"]
+
+    // convert line breaks to html
+    let needsSupportDescHTML = req.session.data["san-"+v+"-"+ref+"-support-desc"].replace(/(?:\r\n|\r|\n)/g, '<br>');
+    // take support strategy data and add it to the prisoner session data
+    let newSupportEntry = {
+      needsSupportCategory: req.session.data["san-"+v+"-"+ref+"-support-category"],
+      needsSupportDescription: needsSupportDescHTML,
+      needsSupportDate: "13 Jun 2025",
+      needsSupportAuthor: "W. Knight"
+    };
+
+    let thisprisoner = req.session.data['prisoners'].find(p => p.prisonerNumber === ref);
+    thisprisoner.needsSupport.push(newSupportEntry);
+    delete req.session.data["san-"+v+"-"+ref+"-support-category"];
+    delete req.session.data["san-"+v+"-"+ref+"-support-desc"];
+
+    res.redirect("/"+v+"/san/"+ref+"/profile");
+  });
+
 
   module.exports = router;
 
