20b5c598829d3eaaafb045a51c91b87a1202635a
Westley Knight <westley.knight1@justice.gov.uk>
2025-09-30 13:03:26 +0100

    Setup of v5 with edit and archive functionality on support strategies (#27)
    * final changes to data for ur
    
    * Update prisoners.json
    
    * Setup of v5 with edit and archive functionality on support strategies

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -4,6 +4,10 @@ let v4prisoners = require('./v4/prisoners.json')
 let v4categories = require('./v4/categories.json')
 let v4conditionlist = require('./v4/conditionlist.json')
 let v4otherconditionlist = require('./v4/otherconditionlist.json')
+let v5prisoners = require('./v5/prisoners.json')
+let v5categories = require('./v5/categories.json')
+let v5conditionlist = require('./v5/conditionlist.json')
+let v5otherconditionlist = require('./v5/otherconditionlist.json')
 
 
 module.exports = {
@@ -14,6 +18,10 @@ module.exports = {
   v4categories,
   v4conditionlist,
   v4otherconditionlist,
-  v4prisoners
+  v4prisoners,
+  v5categories,
+  v5conditionlist,
+  v5otherconditionlist,
+  v5prisoners
 
 }
--- app/routes.js
+++ app/routes.js
@@ -11,3 +11,4 @@ const router = govukPrototypeKit.requests.setupRouter()
 require('./routes/v2.js')(router);
 require('./routes/v3.js')(router);
 require('./routes/v4.js')(router);
+require('./routes/v5.js')(router);
