64dd5ab1248500f577ed5c395ff8c46e5edc1340
JessKissB <156661904+JessKissB@users.noreply.github.com>
2025-10-21 10:44:13 +0100

    Update to cookie
    Session cookie updated

--- app/data/v5/config.js
+++ app/data/v5/config.js
@@ -1,11 +1,13 @@
 // app/config.js
 module.exports = {
-    nunjucksPaths: [
-      'app/views',                               // your views
-      'node_modules/govuk-frontend/',            // GOV.UK
-      'node_modules/govuk-frontend/components',
-      'node_modules/@ministryofjustice/frontend/',        // MOJ
-      'node_modules/@ministryofjustice/frontend/components'
-    ]
-  }
-  
+  nunjucksPaths: [
+    'app/views',                               // your views
+    'node_modules/govuk-frontend/',            // GOV.UK
+    'node_modules/govuk-frontend/components',
+    'node_modules/@ministryofjustice/frontend/',        // MOJ
+    'node_modules/@ministryofjustice/frontend/components'
+  ],
+
+  // 👇 Add this line to store session data in the browser cookie
+  useCookieSessionStore: true
+}
