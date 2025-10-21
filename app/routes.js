15f39dd7ce42d773dda697fb97ebe74253e0b9d6
JessKissB <156661904+JessKissB@users.noreply.github.com>
2025-10-21 08:57:37 +0100

    Rr 1773 v5 edit movetohistory (#30)
    * Changes to edit and archive
    
    Changes to Edit and Archive functionality
    
    * Updates to edit archive and new pages
    
    DPS search and prisoner profile, additional needs tag linking to SAN and updated edit and archive functionality

--- app/routes.js
+++ app/routes.js
@@ -12,3 +12,12 @@ require('./routes/v2.js')(router);
 require('./routes/v3.js')(router);
 require('./routes/v4.js')(router);
 require('./routes/v5.js')(router);
+
+// ✅ Clear session data (works locally and after publishing)
+router.get('/prototype-admin/clear-data', function (req, res) {
+    req.session.destroy(function (err) {
+      res.clearCookie('connect.sid')  // clear cookie in browser
+      res.redirect('/')
+    })
+  })
+  
